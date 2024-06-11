573b0e2d52f3ee6c3f729b9c2ac5fee7e2d8a078
ASHWIN CHANDRASEKARAN <110295167+ashwinchandra08@users.noreply.github.com>
2024-06-11 05:20:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,100 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: "HTML",
+            icon: "html5",
+            color: "#E44D26",
+            percent: 90
+        },
+        {
+            name: "CSS",
+            icon: "css3",
+            color: "#264DE4",
+            percent: 90
+        },
+        {
+            name: "JavaScript",
+            icon: "js",
+            color: "#F0DB4F",
+            percent: 85
+        },
+        {
+            name: "React",
+            icon: "react",
+            color: "#61DAFB",
+            percent: 80
+        },
+        {
+            name: "Node.js",
+            icon: "nodejs",
+            color: "#3C873A",
+            percent: 80
+        },
+        {
+            name: "MongoDB",
+            icon: "mongodb",
+            color: "#47A248",
+            percent: 75
+        },
+        {
+            name: "Express",
+            icon: "express",
+            color: "#000",
+            percent: 70
+        },
+        {
+            name: "Git",
+            icon: "git",
+            color: "#F05032",
+            percent: 80
+        },
+        {
+            name: "GitHub",
+            icon: "github",
+            color: "#181717",
+            percent: 80
+        },
+        {
+            name: "Heroku",
+            icon: "heroku",
+            color: "#430098",
+            percent: 70
+        },
+        {
+            name: "Netlify",
+            icon: "netlify",
+            color: "#00C7B7",
+            percent: 70
+        },
+        {
+            name: "Python",
+            icon: "python",
+            color: "#3776AB",
+            percent: 70
+        },
+        {
+            name: "Django",
+            icon: "django",
+            color: "#092E20",
+            percent: 70
+        },
+        {
+            name: "Bootstrap",
+            icon: "bootstrap",
+            color: "#7952B3",
+            percent: 80
+        },
+        {
+            name: "Materialize",
+            icon: "materialize",
+            color: "#EE6E73",
+            percent: 75
+        },
+        {
+            name: "jQuery",
+            icon: "jquery",
+            color: "#0769AD",
+            percent: 75
+        },
+            ];
+    }
